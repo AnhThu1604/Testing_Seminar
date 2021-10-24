0c1680dbcd318920c128ca3e4fb63e5b623517e8
Anh Thư Phạm <thupham@Anhs-MacBook-Air.local>
2021-10-24 21:45:34 +0700

    edit TeacherDaoTest

--- ProjectHibernate_ver3/src/test/java/dao/TeacherDaoTest.java
+++ ProjectHibernate_ver3/src/test/java/dao/TeacherDaoTest.java
@@ -7,41 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TeacherDaoTest {
 
-    @Test
-    void getTeacherList() {
-    }
-
-    @Test
-    void getTeacher() {
-    }
 
     @Test
     void addTeacher() {
-        Teacher teacher = new Teacher("GV10120", "Pham Thi Anh Thu", "Thu", "0000", "");
+        Teacher teacher = new Teacher("GV1006", "Pham Thi Anh Thu", "Thu", "0000", "");
         assertTrue(TeacherDao.addTeacher(teacher));
     }
 
     @Test
     void addTeacherWithoutId(){
-        Teacher teacher = new Teacher(null, "Nguyen Van A", "VanA", "1111", "");
-        assertTrue(TeacherDao.addTeacher(teacher));
-    }
-
-    @Test
-    void addTeacherWithSameId(){
-        Teacher teacher = new Teacher("GV10120", "Nguyen Van C", "VanC", "9999", "");
+        Teacher teacher = new Teacher("", "Nguyen Van A", "VanA", "1111", "");
         assertFalse(TeacherDao.addTeacher(teacher));
     }
 
     @Test
-    void deleteTeacher() {
+    void addTeacherWithSameId(){
+        Teacher teacher = new Teacher("GV1005", "Nguyen Van C", "VanC", "9999", "");
+        assertFalse(TeacherDao.addTeacher(teacher));
     }
 
-    @Test
-    void updateTeacher() {
-    }
 
-    @Test
-    void fullTextSearch() {
-    }
 }
